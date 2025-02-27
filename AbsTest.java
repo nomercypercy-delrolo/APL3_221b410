7a363adced7a9600f53af7b75cc1c8a6f9702b06
221B410 <221B410@juet.co.in>
2025-02-27 11:25:31 +0530

    Your commit message

---
+++ AbsTest.java
@@ -0,0 +1,5 @@
+public abstract class AbsTest implements Testable {
+    public void display() {
+        System.out.println("Display method in AbsTest class.");
+    }
+}
---
+++ child.java
@@ -0,0 +1,8 @@
+
+
+public class child extends AbsTest {
+public static void main(String[] args) {
+       AbsTest absTest = new child();
+        absTest.display();
+}
+}
